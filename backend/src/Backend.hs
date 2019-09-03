d15bc6ba8d6ad4cba011504b9abed4f16221aa35
Sridhar Ratnakumar <srid@srid.ca>
2019-09-02 21:12:52 -0400

    Add simple UI element, and some route (#1)

--- backend/src/Backend.hs
+++ backend/src/Backend.hs
@@ -1,15 +1,30 @@
-{-# LANGUAGE EmptyCase #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeFamilies #-}
 module Backend where
 
-import Common.Route
+import Data.Dependent.Sum (DSum ((:=>)))
+import Data.Functor.Identity (Identity (..))
+
+import Snap
+
 import Obelisk.Backend
+import Obelisk.Route hiding (decode, encode)
+
+import Common.Route
 
 backend :: Backend BackendRoute FrontendRoute
 backend = Backend
-  { _backend_run = \serve -> serve $ const $ return ()
+  { _backend_run = \serve ->
+      serve requestHandler
   , _backend_routeEncoder = fullRouteEncoder
   }
+  where
+    requestHandler :: MonadSnap m => R BackendRoute -> m ()
+    requestHandler = \case
+      BackendRoute_Missing :=> Identity () -> do
+        writeLBS "404"
+      BackendRoute_AddMood :=> Identity () -> do
+        writeLBS "TODO"
--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -1,18 +1,42 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 module Frontend where
+
+import Data.Text (Text)
 
 import Obelisk.Frontend
 import Obelisk.Route
 import Reflex.Dom.Core
 
+import Obelisk.Generated.Static
 import Common.Route
 
 frontend :: Frontend (R FrontendRoute)
 frontend = Frontend
-  { _frontend_head = el "title" $ text "I"
-  , _frontend_body = do
-      el "h1" $ text "I"
+  { _frontend_head = do
+      elAttr "base" ("href" =: "/") blank
+      elAttr "meta" ("name" =: "viewport" <> "content" =: "width=device-width, initial-scale=1") blank
+      el "title" $ text "I"
+      elAttr "link" ("rel" =: "stylesheet" <> "type" =: "text/css" <> "href" =: static @"semantic.min.css") blank
+  , _frontend_body = divClass "ui container" $ do
+      divClass "ui header" $ text "I"
+      divClass "ui segment" $ do
+        moodE <- moodInput
+        widgetHold_ blank $ ffor moodE $ \mood ->
+          el "pre" $ text mood
   }
+
+-- | Widget for entering a mood for current moment.
+moodInput :: DomBuilder t m => m (Event t Text)
+moodInput = do
+  valD <- divClass "ui input" $
+    value <$> inputElement def
+  addE <- buttonCls "ui primary button" $ text "Add"
+  pure $ tag (current valD) addE
+
+
+-- | Like `button` but using custom CSS classes
+buttonCls :: DomBuilder t m => Text -> m a -> m (Event t ())
+buttonCls cls = fmap (domEvent Click . fst) . elClass' "button" cls
